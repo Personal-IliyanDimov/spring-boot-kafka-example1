18502653b9132d24fe456928f639545e9d126dbb
Personal-IliyanDimov <ilianmd@abv.bg>
2021-01-24 23:47:15 +0200

    Add monitoring functionality

--- src/main/java/org/imd/kafka/sample1/producer/Sample1ProducerApplication.java
+++ src/main/java/org/imd/kafka/sample1/producer/Sample1ProducerApplication.java
@@ -2,11 +2,13 @@ package org.imd.kafka.sample1.producer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.scheduler.Schedulers;
 
 @SpringBootApplication
 public class Sample1ProducerApplication {
 
 	public static void main(String[] args) {
+		Schedulers.enableMetrics();
 		SpringApplication.run(Sample1ProducerApplication.class, args);
 	}
 
