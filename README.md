c8b4582ad401d2ef5e836e931e41972c7cb9e32b
Personal-IliyanDimov <ilianmd@abv.bg>
2021-08-02 10:02:21 -0400

    Small changes

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # spring-boot-kafka-example1
+
+Run producer:
+gradle bootJar -Xmx4096m -Xmx4096m -Dspring.profiles.active=instance1
+
+Run consumer 
+gradle bootJar -Xmx4096m -Xmx4096m -Dspring.profiles.active=instance1  
--- src/test/java/org/imd/kafka/sample1/producer/Sample1ProducerApplicationTests.java
+++ src/test/java/org/imd/kafka/sample1/producer/Sample1ProducerApplicationTests.java
@@ -1,8 +1,10 @@
 package org.imd.kafka.sample1.producer;
 
+import org.junit.Ignore;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Ignore
 @SpringBootTest
 class Sample1ProducerApplicationTests {
 
