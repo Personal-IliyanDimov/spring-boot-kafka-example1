95bec64608ad73f379437b4a6c571859d4c20606
Personal-IliyanDimov <ilianmd@abv.bg>
2021-12-04 16:30:08 +0200

    Enable Http2 protocol on producer side

---
+++ src/main/java/org/imd/kafka/sample1/producer/configuration/SecurityConfiguration.java
@@ -0,0 +1,21 @@
+//package org.imd.kafka.sample1.producer.configuration;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+//import org.springframework.security.config.web.server.ServerHttpSecurity;
+//import org.springframework.security.web.server.SecurityWebFilterChain;
+//
+//@Configuration
+//@EnableWebFluxSecurity
+//public class SecurityConfiguration {
+//
+//    @Bean
+//    SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+//        http.authorizeExchange()
+//            .anyExchange()
+//            .permitAll();
+//
+//        return http.build();
+//    }
+//}
